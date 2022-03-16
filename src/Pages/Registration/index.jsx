7722f8571366ced50482dd97fad3955363c2b83a
mdiallo98 <mamadou1237@gmail.com>
2022-03-16 07:14:49 -0400

    react router

---
+++ src/Pages/Registration/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './style.scss';
+export default class Registration extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Registration Page</h1>
+      </div>
+    );
+  }
+}
