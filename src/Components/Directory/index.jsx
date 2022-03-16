1a4cf33be153a9f83ba2962635f8f4ddb786d07f
mdiallo98 <mamadou1237@gmail.com>
2022-03-16 06:19:09 -0400

    assests pics added

--- src/Components/Directory/index.jsx
+++ src/Components/Directory/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ShopMen from './../../assets/shopMens.jgp';
+import ShopWomens from './../../assets/shopWomens.jgp';
 
 export default function Directory() {
   return <div>Directory</div>;
